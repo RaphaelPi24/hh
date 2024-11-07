89972d30bd5d131e4a49a4f1b970fa85d02996ae
RaphaelPi24 <mymojem1@gmail.com>
2024-11-07 17:56:48 +0300

    Work on the admin block, try2

--- analytics/data_for_diagram.py
+++ analytics/data_for_diagram.py
@@ -1,47 +1,95 @@
+import operator
+from collections import defaultdict
+from functools import reduce
+
+from peewee import fn
+
 from models import VacancyCard
 
 
-def get_popular_skills():
+def get_popular_skills(profession):
+    conditions = [VacancyCard.name.contains(query) for query in profession.split()]
+    # Объединяем условия с оператором & (AND)
+    if conditions:
+        query_conditions = reduce(operator.and_, conditions)
+    else:
+        query_conditions = True  # Если условий нет, оставляем запрос без фильтрации
+
     data = (
         VacancyCard
         .select(VacancyCard.skills)
+        .where(query_conditions)
         .dicts()
     )
     popular_skills = {}
+
     for cart in data:
-        text = cart['skills']
+        text = cart.get('skills')
         if text:
-            skills_list = [skill.strip() for skill in text.split(',') if len(skill) > 0]
+            skills_list = [skill.strip() for skill in text.split(',') if skill.strip()]
             for skill in skills_list:
-                if len(skill) > 0:
-                    if skill in popular_skills:
-                        popular_skills[skill] += 1
-                    else:
-                        popular_skills[skill] = {}
-                        popular_skills[skill] = 1
+                # Увеличиваем счетчик для каждого скилла
+                popular_skills[skill] = popular_skills.get(skill, 0) + 1
+
     return popular_skills
 
+# def get_comparing_skills_with_salary(data: list) -> dict:
+    # keyskills = {}
+    # for cart in data:
+    #     for skill in cart.skills:
+    #         if skill in keyskills:
+    #             keyskills[skill]['count'] += 1
+    #             if cart.get('average_salary') is not None:
+    #                 # Убедись, что список зарплат существует, затем добавь зарплату
+    #                 if 'salary' in keyskills[skill]:
+    #                     keyskills[skill]['salary'].append(cart['average_salary'])
+    #                 else:
+    #                     keyskills[skill]['salary'] = [cart['average_salary']]
+    #         else:
+    #             keyskills[skill] = {}
+    #             keyskills[skill]['count'] = 1
+    #             if cart.get('average_salary') is not None:
+    #                 keyskills[skill]['salary'] = [cart['average_salary']]
+    #             else:
+    #                 keyskills[skill]['salary'] = []
+    # for key in keyskills:
+    #     if len(keyskills[key]['salary']) > 0:
+    #         keyskills[key]['salary'] = sum(keyskills[key]['salary']) // len(keyskills[key]['salary'])
+    # return keyskills
 
-def get_comparing_skills_with_salary(data: list) -> dict:
-    keyskills = {}
+
+def get_comparing_skills_with_salary(profession: str) -> dict:
+
+    conditions = [VacancyCard.name.contains(query) for query in profession.split()]
+    # Объединяем условия с оператором & (AND)
+    if conditions:
+        query_conditions = reduce(operator.and_, conditions)
+    else:
+        query_conditions = True  # Если условий нет, оставляем запрос без фильтрации
+
+    data = (
+        VacancyCard
+        .select(VacancyCard.skills, VacancyCard.average_salary)
+        .where(query_conditions)
+        .dicts()
+    )
+
+    keyskills = defaultdict(lambda: {"count": 0, "salary": []})
+
     for cart in data:
-        for skill in cart.skills:
-            if skill in keyskills:
+        if cart.get('skills') is not None and cart.get('average_salary') is not None:
+            for skill in cart.get('skills').split(','):
+                # Увеличиваем счетчик
                 keyskills[skill]['count'] += 1
-                if cart.get('average_salary') is not None:
-                    # Убедись, что список зарплат существует, затем добавь зарплату
-                    if 'salary' in keyskills[skill]:
-                        keyskills[skill]['salary'].append(cart['average_salary'])
-                    else:
-                        keyskills[skill]['salary'] = [cart['average_salary']]
-            else:
-                keyskills[skill] = {}
-                keyskills[skill]['count'] = 1
-                if cart.get('average_salary') is not None:
-                    keyskills[skill]['salary'] = [cart['average_salary']]
-                else:
-                    keyskills[skill]['salary'] = []
-    for key in keyskills:
-        if len(keyskills[key]['salary']) > 0:
-            keyskills[key]['salary'] = sum(keyskills[key]['salary']) // len(keyskills[key]['salary'])
-    return keyskills
+
+                # Добавляем зарплату
+                keyskills[skill]['salary'].append(cart['average_salary'])
+
+    # Рассчитываем среднюю зарплату для каждого навыка
+    for skill, values in keyskills.items():
+        if values['salary']:
+            values['salary'] = sum(values['salary']) // len(values['salary'])
+        else:
+            values['salary'] = 0  # Или оставьте пустым, если хотите сохранить пустой список
+
+    return dict(keyskills)
--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import asyncio
 import json
-import operator
 import os
 import time
-from functools import reduce
+from collections.abc import Callable
+from typing import Union
 
 import redis
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, render_template, request
 
-from analytics.data_for_diagram import get_popular_skills
-from analytics.diagrams import PopularSkillDiagramBuilder, send
+from analytics.data_for_diagram import get_popular_skills, get_comparing_skills_with_salary
+from analytics.diagrams import PopularSkillDiagramBuilder, send, SkillsSalaryDiagramBuilder
 from models import VacancyCard
 from parsers.easy_parser import get_data, get_average_salary, to_bd, get_keyskills1
 
 app = Flask(__name__)
+redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+names_cache = []
+schedulers = {}
 
 
 @app.route('/', methods=['GET'])
@@ -21,19 +25,69 @@ def get_base():
     return render_template('views/base_content.html')
 
 
-# Сбор данных с помощью формы
-@app.route('/collect_vacancies', methods=['POST'])
-def collect_vacancies():
-    search_query = request.form['input_prof_name']
+def check_images():
+    print("Проверка изображений...")
 
-    vacancy_data = asyncio.run(get_data(search_query))
-    print('Первичные данные собраны')
-    vacancy_data_have_average_salary = get_average_salary(vacancy_data)
-    print('Высчитаны данные вакансий со средними зарплатами')
-    full_vacancy_data = asyncio.run(get_keyskills1(vacancy_data_have_average_salary))
-    print('Собраны полные данные')
-    to_bd(full_vacancy_data)
-    return render_template('views/admin.html', text='Всё прошло успешно')
+    path = r"C:\papka\net\_hh\static\image_diagram"
+    files = os.listdir(path)
+    del_list = []
+    for file in files:
+        name_file = file.split(".")[0]
+        for name_cache in names_cache:
+            if name_file == name_cache:
+                del_list.append(file)
+    for file in del_list:
+        names_cache.remove(file)
+        os.remove(file)
+
+
+# Сбор данных с помощью формы
+@app.route('/admin', methods=['POST'])
+def collect_vacancies():
+    manual_collection = request.form.get('input_prof_name') or None
+    automatic_collection = request.form.get('auto_collect_vacancies') or None
+    timer_for_automatic_collection = request.form.get('timer1') or None
+    timer_for_deleting_images = request.form.get('timer2') or None
+    text = text2 = text3 = ''
+    if manual_collection:
+        process_profession_data(manual_collection)
+        text = 'Сбор успешно завершён'
+    elif automatic_collection:
+        text2 = 'Автосбор успешно запущен'
+        professions_for_autocollection = automatic_collection.split(',')
+        configuring_scheduler(timer_for_automatic_collection, process_profession_data, 'autocollection',
+                              professions_for_autocollection)
+
+    elif timer_for_deleting_images:
+        text3 = 'Таймер успешно запущен'
+        configuring_scheduler(timer_for_deleting_images, check_images, id='delete_images')
+    return render_template('views/admin.html', text=text, text2=text2, text3=text3)
+
+
+@app.route('/stop_main_collection', methods=['POST'])
+def stop_main_collection():
+    stop_scheduler('autocollection')
+    return render_template('views/admin.html', stop_autocollection='Автосбор остановлен')
+
+
+@app.route('/stop_image_cleanup', methods=['POST'])
+def stop_image_cleanup():
+    stop_scheduler('delete_images')
+    return render_template('views/admin.html', stop_delete_images='Автоудаление картинок остановлено')
+
+
+def process_profession_data(professions: [list, str]) -> None:
+    if isinstance(professions, str):
+        professions = [professions]
+    for profession in professions:
+        vacancy_data = asyncio.run(get_data(profession))
+        print('Первичные данные собраны')
+        vacancy_data_have_average_salary = get_average_salary(vacancy_data)
+        print('Высчитаны данные вакансий со средними зарплатами')
+        full_vacancy_data = asyncio.run(get_keyskills1(vacancy_data_have_average_salary))
+        print('Собраны полные данные')
+        to_bd(full_vacancy_data)
+        print(f'Собраны данные по профессии {profession}')
 
 
 # Поиск и отображение диаграммы с помощью HTMX
@@ -45,19 +99,13 @@ def get_show():
     salary_to = request.form.get('salary_to') or None
     city = request.form.get('city') or None
     company = request.form.get('company') or None
-    remote = request.form.get('remote') or None # on - если отмечен.  None
-
-
-
-    #data_for_diagram = get_popular_skills()
-    #diagram = PopularSkillDiagramBuilder()
-    #image = send(diagram, data_for_diagram)
+    remote = request.form.get('remote') or None  # on - если отмечен.  None
 
     start = time.time()
-    redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
     cache_key = full_search_query + str(salary_from) + str(salary_to) + str(city) + str(company) + str(remote)
     cache_data = redis_client.get(cache_key)
-
+    names_cache.append(cache_key)
     if cache_data:
         data_vacancies = json.loads(cache_data)
     else:
@@ -159,21 +207,42 @@ def get_admin():
 
 @app.route('/analytics', methods=['GET'])
 def get_analytics():
-    return render_template('views/diagram.html')
+    return render_template('views/analytics.html')
 
 
-def check_images():
-    while True:
-        print("Проверка изображений...")
-        # Ваш код для проверки наличия изображений
-        path = "путь/к/вашим/изображениям"
-        files = os.listdir(path)
-        for file in files:
-            if file.endswith(".png") or file.endswith(".jpg"):
-                print(f"Изображение найдено: {file}")
+@app.route('/analytics', methods=['POST'])
+def post_analytics():
+    profession = request.form.get('profession') or None
+    profession2 = request.form.get('profession_stats') or None
+    if profession is not None:
+        data_for_diagram = get_popular_skills(profession)
+        diagram = PopularSkillDiagramBuilder()
+    elif profession2 is not None:
+        data_for_diagram = get_comparing_skills_with_salary(profession2)
+        diagram = SkillsSalaryDiagramBuilder()
+    image = send(diagram, data_for_diagram)
+    return render_template('views/diagram.html', image=image)
 
-        # Задержка в 5 минут
-        time.sleep(300)  # 300 секунд = 5 минут
+
+# Настройка планировщика
+def configuring_scheduler(time: str, f: Callable, id: str, params: Union[list, str, None] = None) -> None:
+    time = int(time)
+    if params is not None:
+        params = [params]
+    # job_id = str(uuid.uuid4())
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=f, trigger="interval", minutes=time, args=params, id=id)
+    scheduler.start()
+    schedulers[id] = scheduler
+
+
+def stop_scheduler(id: str) -> None:
+    if id in schedulers:
+        schedulers[id].shutdown()
+        del schedulers[id]
+    else:
+        print("Планировщик с таким идентификатором не найден.")
+
 
 if __name__ == '__main__':
     app.run()
