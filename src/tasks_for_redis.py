300818a72650a24b0b9b0a304a9fe43886838f63
RaphaelPi24 <mymojem1@gmail.com>
2025-10-06 23:25:11 +0300

    deploy5

--- src/tasks_for_redis.py
+++
@@ -1,9 +0,0 @@
-from rq import Queue
-
-from parsers.tasks import process_profession_data
-from cache import Cache
-
-connection = Cache().redis_client
-queue = Queue(connection=connection)
-queue.enqueue(process_profession_data, 'abcdef')
-print('Task created')
